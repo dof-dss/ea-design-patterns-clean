003e678f6b6e9f2675f9b60a6ff975bb29aae69a
alanbailie <alan.bailie@finance-ni.gov.uk>
2019-10-01 15:20:59 +0100

    Added Generic Unit of Work interfaces

---
+++ DSS.Architecture.Patterns.DotNet.Clean/Gateways/IGenericUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace DSS.Architecture.Patterns.DotNet.Clean.Gateways
+{
+    public interface IGenericUnitOfWork<ç>
+    {
+        TRepositoryType GetRepository<TEntity>() where TEntity : class;
+        void Save();
+    }
+}
---
+++ DSS.Architecture.Patterns.DotNet.Clean/Gateways/IGenericUnitOfWorkAsync.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace DSS.Architecture.Patterns.DotNet.Clean.Gateways
+{
+    public interface IGenericUnitOfWorkAsync<TRepositoryType>
+    {
+        TRepositoryType GetRepository<TEntity>() where TEntity : class;
+        Task Save();
+    }
+}
