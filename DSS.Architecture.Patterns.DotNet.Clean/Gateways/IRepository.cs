75b0b2957bd395a4627115d4dde2c95a47877b9e
alanbailie <alan.bailie@finance-ni.gov.uk>
2019-10-02 09:20:09 +0100

    Relaxed use of Generics.

--- DSS.Architecture.Patterns.DotNet.Clean/Gateways/IRepository.cs
+++ DSS.Architecture.Patterns.DotNet.Clean/Gateways/IRepository.cs
@@ -1,21 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace DSS.Architecture.Patterns.DotNet.Clean.Gateways
 {
     /// <summary>
     /// An interface which defines the common members to work with a data store
     /// </summary>
-    /// <typeparam name="T">the type of object the repository wraps</typeparam>
-    public interface IRepository<T>
-        where T : class
+    /// <typeparam name="TEntity">the type of object the repository wraps</typeparam>
+    public interface IRepository<TEntity> 
+        where TEntity : class
     {
         /// <summary>
         /// Creates a new object in the data store.
         /// </summary>
         /// <param name="obj">The object to create within the data store.</param>
-        void Create(T obj);
+        void Create(TEntity obj);
 
         /// <summary>
         /// Deletes an object from the data store.
@@ -27,28 +27,28 @@ namespace DSS.Architecture.Patterns.DotNet.Clean.Gateways
         /// Edits an object in the data store.
         /// </summary>
         /// <param name="obj">The object to edit in the data store.</param>
-        void Edit(T obj);
+        void Edit(TEntity obj);
 
         /// <summary>
         /// Gets an object from the data store.
         /// </summary>
         /// <param name="id">The ID of the object to return from the data store.</param>
         /// <returns>The object matching the specified ID from the data store, or null if none exists.</returns>
-        T Get(int id);
+        TEntity Get(int id);
 
         /// <summary>
         /// Gets all the objects from the data store.
         /// </summary>
         /// <returns>
-        /// An IEnumerable of objects that represent all the objects in the data store.
+        /// An IQueryable of objects that represent all the objects in the data store.
         /// </returns>
-        IEnumerable<T> GetAll();
+        IQueryable<TEntity> GetAll();
 
         /// <summary>
         /// Finds matching objects for the specified filter criteria
         /// </summary>
         /// <param name="filter">a filter which defines the list of items to return</param>
-        /// <returns>an <see cref="IEnumerable{T}"/> of objects</returns>
-        IEnumerable<T> Find(Func<T, bool> filter);
+        /// <returns>a list of objects</returns>
+        IQueryable<TEntity> Find(Func<TEntity, bool> filter);
     }
 }
--- DSS.Architecture.Patterns.DotNet.Clean/Gateways/IRepositoryAsync.cs
+++ DSS.Architecture.Patterns.DotNet.Clean/Gateways/IRepositoryAsync.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace DSS.Architecture.Patterns.DotNet.Clean.Gateways
@@ -7,15 +8,15 @@ namespace DSS.Architecture.Patterns.DotNet.Clean.Gateways
     /// <summary>
     /// An interface which defines the common members to work with a data store
     /// </summary>
-    /// <typeparam name="T">the type of object the repository wraps</typeparam>
-    public interface IRepositoryAsync<T>
-        where T : class
+    /// <typeparam name="TEntity">the type of object the repository wraps</typeparam>
+    public interface IRepositoryAsync<TEntity>
+        where TEntity : class
     {
         /// <summary>
         /// Creates a new object in the data store.
         /// </summary>
         /// <param name="obj">The object to create within the data store.</param>
-        Task Create(T obj);
+        Task Create(TEntity obj);
 
         /// <summary>
         /// Deletes an object from the data store.
@@ -27,14 +28,14 @@ namespace DSS.Architecture.Patterns.DotNet.Clean.Gateways
         /// Edits an object in the data store.
         /// </summary>
         /// <param name="obj">The object to edit in the data store.</param>
-        Task Edit(T obj);
+        Task Edit(TEntity obj);
 
         /// <summary>
         /// Gets an object from the data store.
         /// </summary>
         /// <param name="id">The ID of the object to return from the data store.</param>
         /// <returns>The object matching the specified ID from the data store, or null if none exists.</returns>
-        Task<T> Get(int id);
+        Task<TEntity> Get(int id);
 
         /// <summary>
         /// Gets all the objects from the data store.
@@ -42,13 +43,13 @@ namespace DSS.Architecture.Patterns.DotNet.Clean.Gateways
         /// <returns>
         /// An IEnumerable of objects that represent all the objects in the data store.
         /// </returns>
-        Task<IEnumerable<T>> GetAll();
+        Task<IQueryable<TEntity>> GetAll();
 
         /// <summary>
         /// Finds matching objects for the specified filter criteria
         /// </summary>
         /// <param name="filter">a filter which defines the list of items to return</param>
-        /// <returns>an <see cref="IEnumerable{T}"/> of objects</returns>
-        Task<IEnumerable<T>> Find(Func<T, bool> filter);
+        /// <returns>a list of objects</returns>
+        Task<IQueryable<TEntity>> Find(Func<TEntity, bool> filter);
     }
 }
